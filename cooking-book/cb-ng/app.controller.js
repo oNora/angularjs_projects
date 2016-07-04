0dcf139e768179a661a8e04f87f4dbaf00413ac9
Eleonora Dimchevska <e.dimchevska@gmail.com>
2016-07-04 20:05:11 +0300

    block comments - jsdoc style

--- cooking-book/cb-ng/app.controller.js
+++ cooking-book/cb-ng/app.controller.js
@@ -1,14 +1,34 @@
 (function  () {
+
+    'use strict';
+
+    /**
+     * General controller for CookingBook
+     */
     var app = angular.module("cookingBook");
 
     app.controller("CookingBookController",
                 [ "$scope", '$rootScope',  'cbRecipeService',
                 function($scope, $rootScope, cbRecipeService){
 
+        /**
+         * Get available recipes
+         */
         $rootScope.recipeList = cbRecipeService.getRecipe();
 
+        /**
+         * @type {Boolean} - initial menu status on mobile
+         */
         $scope.isMobileMenu = false;
+
+        /**
+         * @type {String} - initial css class for menu status on mobile
+         */
         $scope.showMobileMenu = 'hideMobile';
+
+        /**
+         * toggle menu status - show and hide on mobile
+         */
         $scope.toggle = function() {
             $scope.isMobileMenu = !$scope.isMobileMenu;
             $scope.showMobileMenu = $scope.isMobileMenu ? 'showMobile' : 'hideMobile';
